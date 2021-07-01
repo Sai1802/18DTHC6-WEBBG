607996507770d9507a39a23e36fcc209d3bbe498
Sai1802 <86455352+Sai1802@users.noreply.github.com>
2021-07-01 19:36:44 +0700

    Initial commit

---
+++ WEBBG/WEBBG/Controllers/SiteController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using WEBBG.Models;
+
+namespace WEBBG.Controllers
+{
+    public class SiteController : Controller
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+
+        private List<SANPHAM> Laysanpham(int count)
+        {
+            return db.SANPHAMs.OrderByDescending(a => a.NGAYSANXUAT).Take(count).ToList();
+            
+        }
+        // GET: Site
+        public ActionResult Index()
+        {
+            var spmoi = Laysanpham(4);
+            return View(spmoi);
+           
+        }
+
+        public ActionResult Product()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+
+    }
+}
